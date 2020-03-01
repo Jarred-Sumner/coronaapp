df8eff2570471e67f0f8f786a7cd56150ca03f3a
Jarred Sumner <jarred@jarredsumner.com>
2020-02-29 18:38:41 -0800

    user reports

--- app/controllers/user_reports_controller.rb
+++ app/controllers/user_reports_controller.rb
@@ -15,18 +15,22 @@ class UserReportsController < ApplicationController
   def create
     report = UserReport.where(device_uid: create_params[:device_uid]).first_or_initialize
     report.location = "POINT(#{create_params[:latitude]} #{create_params[:longitude]})"
-    report.symptoms = create_params[:symptoms]
+    report.symptoms = String(create_params[:symptoms]).split(",")
+    report.save
 
     render json: {success: true}
   end
 
   private def create_params
-    params.require(:user_report).permit([
+    params.require(:user_report).permit(
       :latitude,
       :longitude,
+      :location_accuracy,
       :device_uid,
+      :ip_address,
+      :traveled_recently,
       :symptoms
-    ])
+    )
   end
 
 
